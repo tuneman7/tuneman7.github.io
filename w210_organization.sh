c773fb6b680a22cc9e6ddf7a70071d8e6bf0e586
don <tuneman7>
2022-08-23 12:54:49 -0700

    periodic update

---
+++ w210_organization.sh
@@ -0,0 +1,54 @@
+<html>
+    <body>
+    ********************<br />
+    MIDS Fall 2022<br />
+    ********************<br />
+    W210 Work<br />
+    ********************<br />
+    Team Members:<br />
+    ********************<br />
+    Don Irwin<br />
+
+    <br />
+    Shared Documents:<br />
+    
+    ********************<br />
+    <br />
+    <br/>
+    Recursive Grep:<br/>
+    grep -lrni ./ -e"likelihood" | more
+    <br/>
+    <br/>Looking for file names:<br/>
+    find  ./ -type f -name "*.py"<br/>
+    <br/>
+    git config --global credential.helper store<br/>
+    <br/> 
+    <br/>
+
+    <a href='https://github.com/UCB-W255/main/tree/lab_5/labs/lab_5' target='https://github.com/UCB-W255/main/tree/lab_5/labs/lab_5'>Lab 5</a><br/>
+
+    <a href='https://donirwin.mids-w255.com/docs' target='https://donirwin.mids-w255.com/docs'>https://donirwin.mids-w255.com/docs</a><br/>
+
+    <a href='https://portal.azure.com/#home' target='https://portal.azure.com/#home'>Azure Portal</a><br/>
+
+    <a href='https://portal.azure.com/#@berkeleydatasciw255.onmicrosoft.com/resource/subscriptions/6baae99a-4d64-4071-bfac-c363e71984c3/resourceGroups/w255/providers/Microsoft.ContainerService/managedClusters/w255-aks/overview' target='https://portal.azure.com/#@berkeleydatasciw255.onmicrosoft.com/resource/subscriptions/6baae99a-4d64-4071-bfac-c363e71984c3/resourceGroups/w255/providers/Microsoft.ContainerService/managedClusters/w255-aks/overview'>w255-aks</a><br/>
+
+    <a href='https://github.com/tuneman7/w255_stuff' target='https://github.com/tuneman7/w255_stuff'>W255_stuff_github</a><br/>
+    
+    <a href='https://github.com/UCB-W255/' target='https://github.com/UCB-W255/'>https://github.com/UCB-W255</a><br/>
+
+    
+    <a href='https://learn.datascience.berkeley.edu/login' target='https://learn.datascience.berkeley.edu/login'>ISVC Login</a><br/>    
+
+    <a href='https://corp-prod.s3.us-west-2.amazonaws.com/uploads/47921a85-81bd-4adc-b822-d46f84ca8f36/c25898ce-281c-4fed-987f-327d61732c15/datasci_255_syllabus.pdf?AWSAccessKeyId=AKIAI3SC6Y7T66PAO2FA&Expires=1654574854&Signature=nNTb5xKdHXRyiFMMzbeHIKuE0Pw%3D'  target='https://corp-prod.s3.us-west-2.amazonaws.com/uploads/47921a85-81bd-4adc-b822-d46f84ca8f36/c25898ce-281c-4fed-987f-327d61732c15/datasci_255_syllabus.pdf?AWSAccessKeyId=AKIAI3SC6Y7T66PAO2FA&Expires=1654574854&Signature=nNTb5xKdHXRyiFMMzbeHIKuE0Pw%3D'>Syllabus</a><br/>
+
+    <a href='https://docs.google.com/spreadsheets/d/1gLaFSkdUjBVyoVfqEV8OMzSqRtXVw0-7o8Sm10lbhjQ/edit?usp=sharing'  target='https://docs.google.com/spreadsheets/d/1gLaFSkdUjBVyoVfqEV8OMzSqRtXVw0-7o8Sm10lbhjQ/edit?usp=sharing'>TODO Class Schedule</a><br/>
+
+    <a href='https://berkeley.zoom.us/' target='https://berkeley.zoom.us/'>Berkeley Zoom Login</a><br/>
+
+    <a href='https://berkeley.zoom.us/' target='https://berkeley.zoom.us/'>REDIS</a><br/>
+    
+    
+    </body>
+    
+</html>
